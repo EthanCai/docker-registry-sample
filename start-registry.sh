dda05643ca56298522b37c9b385a57a12a35c693
ethancai <ethancai@qq.com>
2017-03-12 16:28:12 +0800

    ssave

--- start-registry.sh
+++ start-registry.sh
@@ -9,13 +9,6 @@ IFS=$'\n\t'
 # import common.sh
 CUR_DIR=$(cd $(dirname $0) && pwd)
 
-docker run -d -p 5000:5000 --restart=always --name registry \
-  -v $CUR_DIR/data:/var/lib/registry \
-  -v $CUR_DIR/certs:/certs \
-  -e REGISTRY_HTTP_TLS_CERTIFICATE=/certs/all-le.crt \
-  -e REGISTRY_HTTP_TLS_KEY=/certs/all-le.key \
-  -v $CUR_DIR/auth:/auth \
-  -e "REGISTRY_AUTH=htpasswd" \
-  -e "REGISTRY_AUTH_HTPASSWD_REALM=Registry Realm" \
-  -e REGISTRY_AUTH_HTPASSWD_PATH=/auth/htpasswd \
-  registry:2
+pushd $CUR_DIR
+docker-compose up
+popd
